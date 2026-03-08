4690a1e8271ce3af09ca0467a4b682fd9e92d51c
juhairulislam <juhairulislam2018@gmail.com>
2026-03-08 10:25:11 +0600

    design front page dynamically by API

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-
-
-
+// signin functionality
 
 function signin() {
 
@@ -30,3 +28,81 @@ const password = document.getElementById('password');
 
     }
 }
+
+
+
+// loading all card
+
+async function loadingAllCard(){
+
+    const res =await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues") ;
+    const data = await res.json() ;
+    displayCard(data.data) ;
+    console.log(data)
+}
+
+
+async function displayCard(allData){
+
+    const allCardContainer = document.getElementById('all-card-container') ;
+
+    allCardContainer.innerHTML = '' ;
+
+
+    allData.forEach((datum) => {
+
+        const createdCard = document.createElement('div') ;
+        createdCard.className = `card-container shadow rounded border-t-4 ${datum.status === 'open'? 'border-green-500' : 'border-purple-500' }  ` ;
+
+        createdCard.innerHTML = `
+
+          <!-- card head -->
+                <div class=" flex justify-between p-2">
+                    ${datum.status === 'open'? '<img src="./assets/Open-Status.png" alt=""></img>':'<img src="./assets/Closed-Status .png" alt=""></img>' }
+                    <button class="${datum.priority === 'high'? 'bg-red-100 text-red-500' : datum.priority === 'medium'?'bg-[#FFF8DB] text-[#D97706]'  : 'text-[#9CA3AF] bg-[#EEEFF2]' } px-4 rounded-xl  text-[0.8em]">${datum.priority.toUpperCase()}</button>
+                </div>
+
+
+                <!-- card main and labels container -->
+
+              <div class="p-2 ">
+                  <!-- card main -->
+
+                <div class="space-y-3 h-[7.5em] ">
+                    <p class="text-[0.9em] font-semibold">${datum.title}</p>
+                    <p class="text-[0.8em] text-[#64748B]">${datum.description}</p>
+                </div>
+
+                <!-- card labels -->
+
+                <div class="flex gap-1 mt-3">
+                    ${datum.labels?.[0]? `<button class="${datum.labels[0].length >3? 'px-1 py-0.2  border-green-300 bg-green-50 text-green-500' : 'px-3 py-0.3 border-red-300  bg-red-50 text-red-500' } text-[0.7em] rounded-2xl border-2 ">${datum.labels[0].toUpperCase()}</button>` : ''}
+                    ${datum.labels?.[1]? `<button class="${datum.labels[1].length >3? 'px-1 py-0.2 ':'px-3 py-0.3'} rounded-2xl border-2  border-[#FDE68A] text-[0.7em] bg-[#FFF8DB] text-[#D97706]">${datum.labels[1].toUpperCase()}</button>` : ''}
+                </div>
+
+              </div>
+
+                <!-- card footer -->
+
+                 <div class="my-4 border-t-1 border-[#64748B80] p-2">
+                     <p class="text-[#64748B]">${datum.assignee}</p>
+                    <p class="text-[#64748B]">${new Date(datum.createdAt).toLocaleDateString()}</p>
+                </div>
+
+
+        
+        ` ;
+
+        allCardContainer.append(createdCard)
+
+    })
+
+
+
+
+
+}
+
+// displayCard()
+
+loadingAllCard()
